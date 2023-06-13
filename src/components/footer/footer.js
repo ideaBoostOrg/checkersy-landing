9595cd53f6035e744db67e514c9ad6c8a749190f
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2023-06-13 23:28:34 +0530

    Modify footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,42 +6,37 @@ import FooterWidget from 'components/footer-widget';
 import { menuItems, footerNav } from './footer.data';
 import { rgba } from 'polished';
 import Image from 'components/image';
-
-
 import email from 'assets/images/icons/email.webp';
 import visitus from 'assets/images/icons/visitUs.png';
 import linkedin from 'assets/images/icons/linkedin_logo.png';
+import IdeaBoost from 'components/ideaboost'
 
 export default function Footer() {
   return (
     <Box as="footer" variant="layout.footer">
       <Container>
-        {/* <Box sx={styles.footerTopInner}>
-          {menuItems.map(({ id, title, items }) => (
-            <FooterWidget key={id} title={title} items={items} />
-          ))}
-        </Box> */}
       </Container>
       <Container>
         <Box sx={styles.footerInner}>
           <Box sx={styles.copyright}>
             <Logo />
-            {/* <Text as="span">
-              Copyright by {new Date().getFullYear()} RedQ, Inc
-            </Text> */}
+          </Box>          
+          <Box sx={styles.logo}>
+            <IdeaBoost />
+            <Text as="span">
+              Powered by IdeaBoost
+            </Text>
           </Box>
           <Box as="ul" sx={styles.footerNav}>
               <Link path={'contactus@ideaboost.tech'} label={'contactus@ideaboost.tech'} variant="footer" >
                 <Image sx={styles.footerNavImg} src={email} alt="widgets" />
               </Link>
-              <Link path={'https://ideaboost.tech'} variant="footer" sx={styles.link}>
+              <a href={'https://ideaboost.tech'} variant="footer" sx={styles.linkVisit}>
                 <Image sx={styles.footerNavImg} src={visitus} alt="widgets" />
-              </Link>
-            {/* <li sx={styles.footerNavList} >   */}
-              <Link path={'https://www.linkedin.com/company/ideaboosts/'} variant="footer" sx={styles.link}>
+              </a>
+              <a href={'https://www.linkedin.com/company/ideaboosts/'} variant="footer" sx={styles.link}>
                 <Image sx={styles.footerNavImg} src={linkedin} alt="widgets" />
-              </Link>
-            {/* </li> */}
+              </a>
           </Box>    
         </Box>
       </Container>
@@ -61,7 +56,10 @@ const styles = {
       'repeat(3, 1fr)',
       'repeat(5, 1fr)',
     ],
-    
+    '@media only screen and (max-width < 800px)': {
+      display: ['flex'],
+      flexDirection: ['column'],
+    },
   },
   footerInner: {
     borderTop: `1px solid #D9E0E7`,
@@ -83,6 +81,16 @@ const styles = {
       lineHeight: 1.29,
       color: rgba('#0F2137', 0.6),
       mt: ['18px', '18px', '7px'],
+    },
+  },
+  logo: {
+    display: ['flex'],
+    alignItems: 'center',
+    flexDirection: ['column', null, null, null, 'column'],
+    span: {
+      fontSize: '14px',
+      lineHeight: 1.29,
+      color: rgba('#0F2137', 0.6),
     },
   },
   footerNav: {
@@ -118,8 +126,17 @@ const styles = {
   footerCopyRight: {
     ml: '5px',
   },
+  linkVisit:{
+    textDecoration: 'none',
+    fontSize: ' 11pt',
+    mt: ['auto'],
+    mb: ['auto'],
+  },
   link:{
     textDecoration: 'none',
     fontSize: ' 11pt',
+    mt: ['auto'],
+    mb: ['auto'],
+    scale: 1.2,
   }
 };
---
+++ src/components/ideaboost.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import { jsx, Image } from 'theme-ui';
+import { Link } from 'components/link';
+import logo from 'assets/images/ideaboost_logo.png';
+import logoWhite from 'assets/images/logo-white.png';
+
+export default function IdeaBoost({ isWhite }) {
+  return (
+    <Link
+      path="/"
+      sx={{
+        variant: 'links.logo',
+      }}
+    >
+      <Image sx={styles.logo} src={isWhite ? logoWhite : logo} alt="startup landing logo" />
+    </Link>
+  );
+}
+
+const styles = {
+    logo: {
+        scale: ['0.7'],
+    }
+}
